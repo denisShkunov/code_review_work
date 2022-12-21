fe3bbec320c7ca8ff87765e562a295c1ecbf2f2c
PalachBichei <good.mikailov@mail.ru>
2022-12-21 22:47:16 +0300

    update Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,25 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        int sum = a+b;
+        return sum;
     }
     public int dif(int a, int b){
         //TODO inser your realisation in method dif
+        int difference = a-b;
+        return difference;
     }
     public int div(int a, int b){
         //TODO inser your realisation in method div
+        int div = a/b;
+        return div;
     }
     public int times(int a, int b){
         //TODO inser your realisation in method times
+        int times = a*b;
+        return times;
     }
-    public int solver(){
+    public int solver(int a, int b){
         //TODO inser your realisation in method solver
+        return dif(times(a,b), div(a,b));
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
+    Calculator calculator = new Calculator();
     @Test
     void add() {
         //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2,3));
     }
 
     @Test
     void dif() {
         //TODO inser your realisation in test-method dif
+        assertEquals(0,calculator.dif(5,5));
     }
 
     @Test
     void div() {
         //TODO inser your realisation in test-method div
+        assertEquals(1,calculator.div(5,5));
     }
 
     @Test
     void times() {
         //TODO inser your realisation in test-method times
+        assertEquals(25,calculator.times(5,5));
     }
 
     @Test
     void solver() {
         //TODO inser your realisation in test-method solver
+        assertEquals(24,calculator.solver(5,5));
     }
 }
